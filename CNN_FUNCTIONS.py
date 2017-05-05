f36c1b914d6de59ffa1a95dbe82a9ad7af690544
Hong-Ye <hyhu@ucsd.edu>
2017-05-04 17:05:13 -0700

    Added reshapeBatch

--- CNN_FUNCTIONS.py
+++ CNN_FUNCTIONS.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def unpickle(file):
     '''
-    this function reads the CIFAR-10 data
+    Purpose: --- this function reads the CIFAR-10 data
     each batch data contains 10000 pictures
     dict{'data','labels'}
     dict['data'] is a matrix: each row is a picture.
@@ -17,8 +17,28 @@ def unpickle(file):
         dict = cPickle.load(fo)
     return dict
 
+def reshape_batch_data(dic):
+    '''
+    Purpose: --- reshape each picture data from a vector to a 32*32*3 pictures
+    Input: --- dic is the result from unpickle function
+    output: --- reshaped_dic is a dictionary, with 'data' and 'labels'
+                reshaped_dic['data'] is an array with dimension 32*32*3*10000
+    '''
+    number_of_pictures = np.shape(dic['labels'])[0]
+    reshaped_dic = {}
+    # copy the labels from dic to reshaped_dic
+    reshaped_dic['labels'] = dic['labels']
+    # reshape the data
+    reshaped_dic['data'] = np.zeros((32, 32, 3, 10000))
+    for i in range(0, number_of_pictures):
+        for j in range(0,3):
+            reshaped_dic['data'][:,:,j,i] = np.reshape(dic['data'][i,j*1024:(j+1)*1024], (32,32))
+    return reshaped_dic
+    
+
 def conv_layer(pre_layer, filters, moving_step = 1):
     '''
+    Purpose: --- conv_layer is the function doing convolution on the pre_layer, and generate the post_layer
     pre_layer is the input layer before doing convolution. It has dimension w*h*d (width * height * depth)
     filters has dimention r * c * d * number_of_filters(number of row * number of column * depth * number of filters)
     moving_step is the how the left corner of receptive field is moving. 
@@ -47,5 +67,11 @@ def conv_layer(pre_layer, filters, moving_step = 1):
                 # But I don't think that is necessary
                 temp_dot_product = np.multiply(filters[:,:,:,k], pre_layer[j*moving_step:j*moving_step + c, i*moving_step:i*moving_step + r, :])   
                 print(sum(sum(sum(temp_dot_product))))
-                post_layer[j, i, k] = sum(sum(sum(temp_dot_product)))
+                #post_layer[j, i, k] = sum(sum(sum(temp_dot_product)))
+                temp1 = temp_dot_product
+                for p in range(0, d):
+                    temp2 = []
+                    temp2 = sum(temp1)
+                    temp1 = temp2
+                post_layer[j, i, k] = temp1
     return post_layer
